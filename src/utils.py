1ace1731f321fbc2928091a999fee23abe4bcc90
ronit kapar <work.ronitkapar@gmail.com>
2026-05-28 00:04:48 +0530

    completed project

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 from tkinter import Grid 
 import dill  # type: ignore
 
@@ -54,4 +55,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             return report
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
